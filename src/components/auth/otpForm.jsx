25f6eabb71c2700b76caed67d32e8d596773dcc5
Aman Gupta <141570525+imamangupta@users.noreply.github.com>
2024-12-22 00:05:32 +0530

    localhsot set

--- src/components/auth/otpForm.jsx
+++ src/components/auth/otpForm.jsx
@@ -37,7 +37,7 @@ export default function OTPForm() {
     if (json.message) {
         
 
-        const response2 = await fetch(`http://localhost:4000/signup`, {
+        const response2 = await fetch(`${BaseApiUrl}/signup`, {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json'
